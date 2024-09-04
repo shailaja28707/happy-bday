81d6e732659db7210dd578af310f363b61a3e974
shailaja28707 <swamyshailaja880@gmail.com>
2024-09-04 22:55:17 +0530

    Add files via upload

---
+++ Bday.js
@@ -0,0 +1,5 @@
+document.getElementById("bdayButton").addEventListener("click", function() {
+    var messageElement = document.getElementById("message");
+    messageElement.textContent = "Happy Birthday! 🎉";
+    messageElement.style.color = "#FF6347";
+});
